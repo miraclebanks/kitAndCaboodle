0f742563b242a9845564ec19b15d8501fb1065a4
miraclebanks <miraclebnks@gmail.com>
2020-09-01 18:43:16 -0700

    Updates

---
+++ precache-manifest.7567e2af44fdac4aeab960c069194977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2516153a6e00e0f589f372d1d4ecdde",
+    "url": "/kitAndCaboodle/index.html"
+  },
+  {
+    "revision": "ebb1f34176342be7e0dc",
+    "url": "/kitAndCaboodle/static/css/main.696a6fa0.chunk.css"
+  },
+  {
+    "revision": "10114f2f76bbf9b2c006",
+    "url": "/kitAndCaboodle/static/js/2.a8d0fd7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kitAndCaboodle/static/js/2.a8d0fd7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb1f34176342be7e0dc",
+    "url": "/kitAndCaboodle/static/js/main.50275568.chunk.js"
+  },
+  {
+    "revision": "bd880c1aa03344447bc5",
+    "url": "/kitAndCaboodle/static/js/runtime-main.b5517790.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kitAndCaboodle/static/media/logo.5d5d9eef.svg"
+  }
+]);
